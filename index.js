89e0bbd5c94cc7c50c7bb9e94fedbbeb08884303
laurenah <lozhambo@gmail.com>
2020-08-23 21:22:11 +1000

    fix css/svg rendering - move css/img file to public folder

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(__dirname + '/index.html');
 });
 
 // login page
